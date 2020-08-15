561e29f33724ea064d2dc1cf8d48cebbbf0d47ee
kekoyana <kekoyana0@gmail.com>
2020-08-16 01:19:10 +0900

    エラー文言の日本語対応

--- app/validators/kana_validator.rb
+++ app/validators/kana_validator.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class KanaValidator < ActiveModel::EachValidator
-  KANA_REGEX = /\A[0-9ぁ-んー－]+\z/.freeze
+  KANA_REGEX = /\A[0-9ぁ-んー－]*\z/.freeze
 
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, 'invalid kana format') if value.match(KANA_REGEX).blank?
+    return if value.nil?
+
+    record.errors.add(attribute, :kana) if value.match(KANA_REGEX).blank?
   end
 end
