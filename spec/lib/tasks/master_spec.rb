bd6080e83c7d5c89266a7a066113145387076cae
kekoyana <kekoyana0@gmail.com>
2020-08-15 12:36:02 +0900

    master:fetchのtaskの追加

---
+++ spec/lib/tasks/master_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'rake'
+
+describe 'rake task master' do
+  before do
+    Rake.application.tasks.each(&:reenable)
+  end
+
+  describe 'master:fetch' do
+    subject(:task) { Rake.application['master:fetch'] }
+
+    it 'is succeed.' do
+      expect(task.invoke).to be_truthy
+    end
+  end
+end
